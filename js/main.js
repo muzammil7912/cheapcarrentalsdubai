d645539b91312d629d503990c17b25260ccf1c96
Muzammil React(Developer) <79418859+muzammil7912@users.noreply.github.com>
2023-11-01 17:51:03 +0500

    mu

---
+++ js/main.js
@@ -0,0 +1,40 @@
+var Swipes = new Swiper('.swiper-container', {
+    speed: 500,
+    effect: "coverflow",
+    grabCursor: true,
+    centeredSlides: true,
+    slidesPerView: "auto",
+    coverflowEffect: {
+      rotate: 50,
+      stretch: 0,
+      depth: 100,
+      modifier: 1,
+      slideShadows: true,
+    },
+    autoplay: {
+        delay: 10000,
+        disableOnInteraction: false,
+    },
+    pagination: {
+        el: ".swiper-pagination",
+        clickable: true,
+    },
+});
+
+
+$(".ta1 .section5__left-list-Box").click(function() {
+    if(!$(this).hasClass("active")) {
+        $(".ta1 .section5__left-list-Box").removeClass("active")
+        $(".ta1 .section5__left-Body-Box").slideUp()
+        $(this).addClass("active")
+        $(`#${$(this).data("tab")}`).slideDown()
+    }
+})
+$(".ta2 .section5__left-list-Box").click(function() {
+    if(!$(this).hasClass("active")) {
+        $(".ta2 .section5__left-list-Box").removeClass("active")
+        $(".ta2 .section5__left-Body-Box").slideUp()
+        $(this).addClass("active")
+        $(`#${$(this).data("tab")}`).slideDown()
+    }
+})
